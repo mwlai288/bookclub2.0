c42b0d5219ab86256abf9fbe36bc5aced3fdc8c3
Mateus Lai <mwlai288@gmail.com>
2020-09-12 22:04:10 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+// const connectDB = require("./config/db");
+// const users = require("./routes/users");
+// const profile = require("./routes/profile");
+// const posts = require("./routes/posts");
+// const auth = require("./routes/auth");
+
+const app = express();
+
+// Body parser middleware
+app.use(express.json({ extended: false }));
+
+// Connect to MongoDB
+// connectDB();
+
+// Use Routes
+// app.use("/api/users", users);
+// app.use("/api/auth", auth);
+// app.use("/api/profile", profile);
+// app.use("/api/posts", posts);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Magic happening on port ${PORT}`);
+});
