6a4ada7a83c0aa55c59f5cf7ad4e3ffa9921cf61
Mateus Lai <mwlai288@gmail.com>
2020-09-14 22:40:01 -0400

    normalize url

--- routes/profile.js
+++ routes/profile.js
@@ -3,8 +3,7 @@ const config = require("config");
 const router = express.Router();
 const auth = require("../middleware/auth");
 const { check, validationResult } = require("express-validator");
-// bring in normalize to give us a proper url, regardless of what user entered
-const normalize = require("normalize-url");
+
 const checkObjectId = require("../middleware/checkObjectId");
 
 const Profile = require("../models/Profile");
