cee880502d5db1085e6c7774679013395012c518
Mateus Lai <mwlai288@gmail.com>
2020-09-14 22:22:20 -0400

    prepare for deployment

--- server.js
+++ server.js
@@ -19,6 +19,16 @@ app.use("/api/auth", auth);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Magic happening on port ${PORT}`);
